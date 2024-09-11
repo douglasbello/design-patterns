43bc7db691656c418308330dd4f24462fc2e4a79
Douglas Bello <dgbello.dev@hotmail.com>
2024-09-11 09:25:57 -0300

    feat: adapter design pattern demo

---
+++ adapter/src/main/java/com/douglasbello/adapters/SquarePegAdapter.java
@@ -0,0 +1,20 @@
+package com.douglasbello.adapters;
+
+import com.douglasbello.round.RoundPeg;
+import com.douglasbello.square.SquarePeg;
+
+public class SquarePegAdapter extends RoundPeg {
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+
+        return result;
+    }
+}
